6429697a3aeb85fb3fa2fe3668927ad4bf13d8cd
Pedro henrique Dias de Souza <152524959+PedroFront3@users.noreply.github.com>
2024-09-27 14:44:08 -0300

    BackGround

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function trocarCor(cor) {
+    document.body.style.backgroundImage = 'none'
+    document.body.style.backgroundColor = cor;
+}
+
+function corAleatoria() {
+    const red = Math.floor(Math.random()*255);
+    const green = Math.floor(Math.random()*255);
+    const blue = Math.floor(Math.random()*255);
+
+    document.body.style.backgroundImage = 'none'
+
+    document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+
+}
+
+function aplicarCorPersonalizada() {
+   const corInput = document.querySelector('.input-color').value 
+
+   trocarCor(corInput);
+}
+
+function escolherImagem(imagem) {
+    const reader = new FileReader()
+    
+    reader.onload = function(evento) {
+       const urlImagem = evento.target.result
+
+       document.body.style.backgroundImage = `url('${urlImagem}')`
+    }
+
+    reader.readAsDataURL(imagem)
+}
